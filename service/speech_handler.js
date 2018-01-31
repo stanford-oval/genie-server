d046b73556a1d07e5785794ec7131a9e30feaf44
Giovanni Campagna <gcampagn@cs.stanford.edu>
2018-01-31 10:14:36 -0800

    Update Copyright notices
    After rereading the University Copyright Agreement, it turns out
    that the university owns the copyright of this work (because it
    is sponsored research). This has been the case from the beginning,
    and previous copyright notices were factually wrong, so this is
    not a change or a relicensing.

--- service/speech_handler.js
+++ service/speech_handler.js
@@ -1,6 +1,8 @@
 // -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
 //
-// Copyright 2017 Giovanni Campagna <gcampagn@cs.stanford.edu>
+// Copyright 2017 The Board of Trustees of the Leland Stanford Junior University
+//
+// Author: Giovanni Campagna <gcampagn@cs.stanford.edu>
 //
 // See COPYING for details
 "use strict";
