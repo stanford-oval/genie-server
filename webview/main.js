608a9228d2a336dc096b1ea1369cd06b12cd1c1f
Giovanni Campagna <gcampagn@cs.stanford.edu>
2018-03-19 16:10:20 -0700

    Add a simple WebkitGtk-based webview
    Using a custom browser allows handling of redirects for devices
    that really don't like doing OAuth locally, like Facebook or Nest.

---
+++ webview/main.js
@@ -0,0 +1,107 @@
+// -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
+//
+// This file is part of Almond
+//
+// Copyright 2018 The Board of Trustees of the Leland Stanford Junior University
+//
+// Author: Giovanni Campagna <gcampagn@cs.stanford.edu>
+//
+// See COPYING for details
+"use strict";
+
+const System = imports.system;
+const GObject = imports.gi.GObject;
+const GLib = imports.gi.GLib;
+const Gio = imports.gi.Gio;
+const Gtk = imports.gi.Gtk;
+const Gdk = imports.gi.Gdk;
+const WebKit = imports.gi.WebKit2;
+
+const BrowserWindow = GObject.registerClass(class BrowserWindow extends Gtk.ApplicationWindow {
+    _init(app) {
+        super._init({ application: app, title: "Almond" });
+
+        let webview = new WebKit.WebView({
+            web_context: app.webContext,
+            hexpand: true,
+            vexpand: true,
+        });
+        webview.connect('decide-policy', (webView, decision, decisionType) => {
+            if (decisionType !== WebKit.PolicyDecisionType.NAVIGATION_ACTION)
+                return false;
+
+            let uri = decision.request.uri;
+            if (uri.startsWith('https://thingengine.stanford.edu/devices/oauth2/callback')) {
+                let path = uri.substring('https://thingengine.stanford.edu'.length);
+                decision.ignore();
+
+                log('Got redirect to ' + uri);
+                webview.load_uri('http://127.0.0.1:3000' + path);
+                return true;
+            }
+
+            return false;
+        });
+        webview.load_uri('http://127.0.0.1:3000');
+
+        webview.show();
+
+        let refreshAction = new Gio.SimpleAction({ name: 'refresh' });
+        refreshAction.connect('activate', () => {
+            webview.reload();
+        });
+        let refreshNoCacheAction = new Gio.SimpleAction({ name: 'refresh-nocache' });
+        refreshNoCacheAction.connect('activate', () => {
+            webview.reload_bypass_cache();
+        });
+        this.add_action(refreshAction);
+        this.add_action(refreshNoCacheAction);
+
+        this.application.set_accels_for_action('win.refresh', ['F5']);
+        this.application.set_accels_for_action('win.refresh-nocache', ['<Shift>F5']);
+
+        this.add(webview);
+        this.fullscreen();
+        this.show();
+    }
+});
+
+const EmbeddedBrowserApplication = GObject.registerClass(class EmbeddedBrowserApplication extends Gtk.Application {
+    vfunc_startup() {
+        super.vfunc_startup();
+
+        let quitAction = new Gio.SimpleAction({ name: 'quit' });
+        quitAction.connect('activate', () => {
+            let win = this.get_active_window();
+            if (win)
+                win.destroy();
+        });
+        this.add_action(quitAction);
+
+        let webDataManager = new WebKit.WebsiteDataManager({
+            base_cache_directory: GLib.get_user_cache_dir() + '/almond-server/webview',
+            base_data_directory: GLib.get_user_config_dir() + '/almond-server/webview'
+        });
+        let webCookieManager = webDataManager.get_cookie_manager();
+        webCookieManager.set_accept_policy(WebKit.CookieAcceptPolicy.NO_THIRD_PARTY);
+        webCookieManager.set_persistent_storage(GLib.get_user_config_dir() + '/almond-server/webview/cookies.db',
+                                                WebKit.CookiePersistentStorage.SQLITE);
+        this.webContext = new WebKit.WebContext({
+            website_data_manager: webDataManager
+        });
+    }
+
+    vfunc_activate() {
+        let window = this.get_active_window();
+        if (window === null)
+            window = new BrowserWindow(this);
+        else
+            window.present();
+    }
+});
+
+function main() {
+    const application = new EmbeddedBrowserApplication();
+    return application.run([System.programInvocationName].concat(ARGV));
+}
+main();
