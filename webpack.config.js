f4b35a66adcd26a3b83cb5a5473c0d4c6d1f7c60
Euirim Choi <euirim@me.com>
2020-02-20 11:00:29 -0800

    Remove unnecessary comments and spacing

--- webpack.config.js
+++ webpack.config.js
@@ -20,18 +20,14 @@ module.exports = {
     path: path.resolve(__dirname, 'react_build'),
     filename: 'bundle.js'
   },
-
   resolve: {
     extensions: ['.ts', '.tsx', '.js', 'jsx']
   },
-
   module: {
     rules: [
       {
         test: /\.(ts|js)x?$/,
-        // ignore transpiling JavaScript from node_modules as it should be that state
         exclude: /node_modules/,
-        // use the babel-loader for transpiling JavaScript to a suitable format
         use: {
           loader: 'babel-loader'
         }
@@ -53,7 +49,6 @@ module.exports = {
       }
     ]
   },
-  // add a custom index.html as the template
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'react', 'index.html')
