3978008485f7a01692b785bc291f3d1b97d253c2
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-04-03 10:59:20 -0700

    Restore config to point to production NLP server

--- config.js
+++ config.js
@@ -56,7 +56,7 @@ module.exports.HOST_BASED_AUTHENTICATION = process.env.THINGENGINE_HOST_BASED_AU
 module.exports.ENABLE_DB_ENCRYPTION = false;
 
 
-module.exports.SEMPRE_URL = 'https://nlp-staging.almond.stanford.edu';
+module.exports.SEMPRE_URL = 'https://almond-nl.stanford.edu';
 module.exports.THINGPEDIA_URL = 'https://thingpedia.stanford.edu/thingpedia';
 module.exports.MS_SPEECH_RECOGNITION_PRIMARY_KEY = 'de1f02817356494483ba502b2ce95f6f';
 module.exports.MS_SPEECH_RECOGNITION_SECONDARY_KEY = '3dc6ce0b832940f0b0c984a1517c457e';
