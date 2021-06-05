c91904a32f37799d73f6b0339b817ef279bfff3f
Giovanni Campagna <gcampagn@cs.stanford.edu>
2021-06-05 14:05:29 -0700

    Add support for redirects inside Home Assistant add-ons
    If the location of the parent frame includes the "almond_redirect"
    query parameter, redirect the Almond frame to that URL. This
    allows to deep link inside the add-on by linking to the main add-on
    page with the "almond_redirect" query param.

--- public/javascripts/shared.js
+++ public/javascripts/shared.js
@@ -1,7 +1,30 @@
+"use strict";
 (function() {
     window.Almond = {
         getThingpedia: function() {
             return 'https://thingpedia.stanford.edu';
         }
     };
+
+    const top = window.parent.location;
+    if (!top)
+        return;
+    const params = new URLSearchParams(top.search.substring(1));
+    if (params.has('almond_redirect')) {
+        const parsed = new URL(params.get('almond_redirect'), window.location.href);
+
+        // for security reasons, we ignore the protocol/hostname/port
+        // and only allow safe-listed paths
+        if (!parsed.pathname.startsWith('/devices/oauth2/callback/'))
+            return;
+
+        let redirectTo = parsed.pathname;
+        for (const [key, value] of params) {
+            if (key === 'almond_redirect')
+                continue;
+            parsed.searchParams.append(key, value);
+        }
+        redirectTo += '?' + parsed.searchParams;
+        window.location.href = redirectTo;
+    }
 })();
