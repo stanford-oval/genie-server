b639b00c6971c33ce65876c1d66acd7131a481a8
Giovanni Campagna <gcampagn@cs.stanford.edu>
2021-11-29 16:53:36 -0800

    Update dependencies and genie-toolkit
    And adjust for the changes in how we store the conversation state
    persistently.

--- src/routes/recording.ts
+++ src/routes/recording.ts
@@ -22,6 +22,8 @@ import express from 'express';
 
 import * as user from '../util/user';
 
+import * as Config from '../config';
+
 const router = express.Router();
 
 router.use(user.requireLogIn);
@@ -29,15 +31,18 @@ router.use(user.requireLogIn);
 router.post('/start', (req, res, next) => {
     const engine = req.app.genie;
 
-    Promise.resolve().then(() => {
-        return engine.assistant.getConversation(req.body.id);
-    }).then((conversation) => {
+    Promise.resolve().then(async () => {
+        const conversation = await engine.assistant.getOrOpenConversation(req.body.id, Config.CONVERSATION_OPTIONS);
         if (!conversation) {
             res.status(404);
             return res.json({ error: 'No conversation found' });
         } else {
-            conversation.startRecording();
-            return res.json({ status: 'ok' });
+            try {
+                await conversation.startRecording();
+                return res.json({ status: 'ok' });
+            } finally {
+                await engine.assistant.closeConversation(conversation.id);
+            }
         }
     }).catch(next);
 });
@@ -45,15 +50,18 @@ router.post('/start', (req, res, next) => {
 router.post('/stop', (req, res, next) => {
     const engine = req.app.genie;
 
-    Promise.resolve().then(() => {
-        return engine.assistant.getConversation(req.body.id);
-    }).then((conversation) => {
+    Promise.resolve().then(async () => {
+        const conversation = await engine.assistant.getOrOpenConversation(req.body.id, Config.CONVERSATION_OPTIONS);
         if (!conversation) {
             res.status(404);
             return res.json({ error: 'No conversation found' });
         } else {
-            conversation.endRecording();
-            return res.json({ status:'ok' });
+            try {
+                await conversation.endRecording();
+                return res.json({ status: 'ok' });
+            } finally {
+                await engine.assistant.closeConversation(conversation.id);
+            }
         }
     }).catch(next);
 });
@@ -61,33 +69,42 @@ router.post('/stop', (req, res, next) => {
 router.get('/status/:id', (req, res, next) => {
     const engine = req.app.genie;
 
-    Promise.resolve().then(() => {
-        return engine.assistant.getConversation(req.params.id);
-    }).then((conversation) => {
+    Promise.resolve().then(async () => {
+        const conversation = await engine.assistant.getOrOpenConversation(req.body.id, Config.CONVERSATION_OPTIONS);
         if (!conversation) {
             res.status(404);
-            res.json({ error: 'No conversation found' });
+            return res.json({ error: 'No conversation found' });
         } else {
-            res.json({ status: conversation.inRecordingMode ? 'on' : 'off' });
+            try {
+                return res.json({ status: conversation.inRecordingMode ? 'on' : 'off' });
+            } finally {
+                await engine.assistant.closeConversation(conversation.id);
+            }
         }
     }).catch(next);
 });
 
 router.post('/vote/:vote', (req, res, next) => {
     const engine = req.app.genie;
+    const vote = req.params.vote;
+    if (vote !== 'up' && vote !== 'down') {
+        res.status(400);
+        res.json({ error: 'Invalid voting option' });
+        return;
+    }
 
-    Promise.resolve().then(() => {
-        return engine.assistant.getConversation(req.body.id);
-    }).then((conversation) => {
-        if (req.params.vote !== 'up' && req.params.vote !== 'down') {
-            res.status(400);
-            return res.json({ error: 'Invalid voting option' });
-        } else if (!conversation) {
+    Promise.resolve().then(async () => {
+        const conversation = await engine.assistant.getOrOpenConversation(req.body.id, Config.CONVERSATION_OPTIONS);
+        if (!conversation) {
             res.status(404);
             return res.json({ error: 'No conversation found' });
         } else {
-            conversation.voteLast(req.params.vote);
-            return res.json({ status:'ok' });
+            try {
+                await conversation.voteLast(vote);
+                return res.json({ status:'ok' });
+            } finally {
+                await engine.assistant.closeConversation(conversation.id);
+            }
         }
     }).catch(next);
 });
@@ -100,15 +117,18 @@ router.post('/comment', (req, res, next) => {
         return;
     }
 
-    Promise.resolve().then(() => {
-        return engine.assistant.getConversation(req.body.id);
-    }).then((conversation) => {
+    Promise.resolve().then(async () => {
+        const conversation = await engine.assistant.getOrOpenConversation(req.body.id, Config.CONVERSATION_OPTIONS);
         if (!conversation) {
             res.status(404);
             return res.json({ error: 'No conversation found' });
         } else {
-            conversation.commentLast(req.body.comment);
-            return res.json({ status:'ok' });
+            try {
+                await conversation.commentLast(req.body.comment);
+                return res.json({ status:'ok' });
+            } finally {
+                await engine.assistant.closeConversation(conversation.id);
+            }
         }
     }).catch(next);
 });
@@ -116,17 +136,18 @@ router.post('/comment', (req, res, next) => {
 router.get('/log/:id.txt', (req, res, next) => {
     const engine = req.app.genie;
 
-    Promise.resolve().then(() => {
-        const conversation = engine.assistant.getConversation((req.params as any).id);
+    Promise.resolve().then(async () => {
+        const conversation = await engine.assistant.getOrOpenConversation(req.body.id, Config.CONVERSATION_OPTIONS);
         if (!conversation) {
             res.status(404);
-            res.send('No conversation found');
+            res.json({ error: 'No conversation found' });
             return;
         }
 
         res.set('Content-Type', 'text/plain');
         res.set('Content-Disposition', `attachment; filename="log-${conversation.id}.txt"`);
         conversation.readLog().pipe(res);
+        await engine.assistant.closeConversation(conversation.id);
     }).catch(next);
 });
 
